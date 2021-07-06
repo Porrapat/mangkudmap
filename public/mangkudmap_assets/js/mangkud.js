9fa1f7d08196f4afc326a4da9b6cfee170c9a613
Porrapat Petchdamrongskul <porrapat.cpe20@gmail.com>
2021-07-06 11:59:40 +0700

    Add all sb-admin-2 assets.

---
+++ public/mangkudmap_assets/js/mangkud.js
@@ -0,0 +1,24 @@
+// jQuery(document).ready(function(){
+    
+      
+//     jQuery('#mapcanvas')
+//       .gmap3({
+//         center:[48.8620722, 2.352047],
+//         zoom:4
+//       });
+//     //   .marker([
+//     //     {position:[48.8620722, 2.352047]},
+//     //     {address:"86000 Poitiers, France"},
+//     //     {address:"66000 Perpignan, France", icon: "https://maps.google.com/mapfiles/marker_grey.png"}
+//     //   ])
+//     //   .on('click', function (marker) {
+//     //     marker.setIcon('https://maps.google.com/mapfiles/marker_green.png');
+//     //   });
+// });
+
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById("mapcanvas"), {
+//         center: { lat: -34.397, lng: 150.644 },
+//         zoom: 8,
+//     });
+// }
